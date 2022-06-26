56ce77bc073245e797309257b0511e1175e6e4eb
JATIN YADAV <jatinyadav0000@gmail.com>
2022-06-26 15:40:48 +0530

    gcd iteration

---
+++ Recursion/gcdinpy.py
@@ -0,0 +1,18 @@
+# find the GCD of two numbers
+
+# Using Iterations
+"""
+def gcdofnum(a,b):
+    if a<b:
+        small = a
+    else:
+        small = b
+
+    for i in range(1,small+1//2):
+        if a%i == 0 and b%i == 0 :
+            gcd = i
+    return gcd
+
+print(gcdofnum(48,60))
+"""
+
